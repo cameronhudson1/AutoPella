7805a9d3de3e23cd7cf97ec0daa288ddb7a1154b
cmrnhudson@gmail.com <cameronhudson@camerons-mbp.wireless.rit.edu>
2019-03-07 17:43:16 -0500

    Add Notes

--- Wav.c
+++ Wav.c
@@ -7,11 +7,18 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "Wav.h"
+#include "Notes.h"
 
 WavFile* createEmtpyWavFile(){
 	WavFile * wav = NULL;
 	
 	return wav; 
+}
+
+WavFile* createWavFile(){
+	WavFile * wav = NULL;
+
+	return wav;
 } 
 
 int main(){
